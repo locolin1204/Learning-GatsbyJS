7b64a492778334979ab2898eac627a0c43096e8b
locolin <locolin99@gmail.com>
2022-06-18 16:38:24 +0800

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "../components/layout";
 
@@ -7,6 +8,10 @@ const AboutPage = () => {
 			<p>
 				Hi there! I'm the proud creator of this site, which I built with Gatsby.
 			</p>
+			<StaticImage
+				alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+				src="../images/clifford.jpg"
+			/>
 		</Layout>
 	);
 };
